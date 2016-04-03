46935385fd7b3a2e3766563eb1f94528bf4b5844
First Wanderer <kaa-master37@mail.ru>
2016-04-04 00:50:40 +0300

    Right constructor of object

--- jquery-exercise/script.js
+++ jquery-exercise/script.js
@@ -1,18 +1,36 @@
 var $ = (function(selector) {
   'use strict';
+
   var jQuery = function(selector) {
-    return document.querySelectorAll(selector);
+    var elements = document.querySelectorAll(selector);
+
+    for (var i = 0, n = elements.length; i < n; i++) {
+      this[i] = elements[i];
+    }
+
+    this.length = elements.length;
   };
 
-  jQuery.prototype.each = function() {
-    console.log('Hello World');
+  jQuery.prototype.each = function(fn) {
+    for (var i = 0, n = this.length; i < n; i++) {
+      fn.call(this, this[i], i, this);
+    }
+
+    return this;
   };
 
   var $$ = function(selector) {
     return new jQuery(selector);
   };
 
+  $$.fn = jQuery.prototype;
+
   return $$;
 })();
 
 console.log ($('.add'));
+
+$('.add').each(function(element, index, collection) {
+  console.log(index);
+  element.textContent = 'This is element #' + index;
+});
