cd4342c1ede7fb88c9af92b7b00c3f0f2f29e17f
First Wanderer <kaa-master37@mail.ru>
2016-04-05 01:07:06 +0300

    Add methods css, on, off

--- jquery-exercise/script.js
+++ jquery-exercise/script.js
@@ -19,6 +19,40 @@ var $ = (function(selector) {
     return this;
   };
 
+  jQuery.prototype.css = function(cssRule, cssValue) {
+    if (typeof cssRule === 'string') {
+      this.each(function(element) {
+        element.style[cssRule] = cssValue;
+      });
+    }
+
+    if (typeof cssRule === 'object') {
+      this.each(function(element) {
+        for (var key in cssRule) {
+          element.style[key] = cssRule[key];
+        }
+      });
+    }
+
+    return this;
+  };
+
+  jQuery.prototype.on = function(event, handler) {
+    this.each(function(element) {
+      element.addEventListener(event, handler);
+    });
+
+    return this;
+  };
+
+  jQuery.prototype.off = function(event, handler) {
+    this.each(function(element) {
+      element.removeEventListener(event, handler);
+    });
+
+    return this;
+  };
+
   var $$ = function(selector) {
     return new jQuery(selector);
   };
@@ -32,5 +66,25 @@ console.log ($('.add'));
 
 $('.add').each(function(element, index, collection) {
   console.log(index);
-  element.textContent = 'This is element #' + index;
+  element.textContent = 'This is element #' + index + ' Click me!';
 });
+
+$('.add').css('background', 'red');
+
+$('.second').css({
+  'marginTop': '10px',
+  'border': '1px solid #ccc'
+});
+
+$('.add').on('click', function(e) {
+  e.preventDefault();
+  console.log('Element was clicked');
+});
+
+var fn = function() {
+  console.log('this event will be removed');
+};
+
+$('#dec').on('change', fn);
+
+$('#dec').off('change', fn);
